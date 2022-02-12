e82ae005b8e399843c22a9f95d4dc9c157a6fffb
Ubuntu <ubuntu@ip-172-31-15-53.ap-northeast-1.compute.internal>
2022-02-12 10:20:41 +0000

    verrr

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,40 +1,39 @@
 class TasksController < ApplicationController
-    def index
-        @tasks = Task.all
+  def index
+      @tasks = Task.all
   end
 
   def show
-      @tasks = Task.find(params[:id])
+      @task = Task.find(params[:id])
   end
 
   def new
-      @tasks = Task.new
+      @task = Task.new
   end
 
   def create
-      @tasks = Task.new(tasks_params)
+      @task = Task.new(task_params)
 
-    if @tasks.save
+    if @task.save
       flash[:success] = 'task が正常に投稿されました'
-      redirect_to @tasks
+      redirect_to @task
     else
       flash.now[:danger] = 'task が投稿されませんでした'
       render :new
+    end
   end
-end
   def edit
-      def edit
-    @tasks = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
-  end
+  
   
 
   def update
-      @tasks = Task.find(params[:id])
+      @task = Task.find(params[:id])
 
-    if @tasks.update(tasks_params)
+    if @task.update(task_params)
       flash[:success] = 'task は正常に更新されました'
-      redirect_to @tasks
+      redirect_to @task
     else
       flash.now[:danger] = 'task は更新されませんでした'
       render :edit
@@ -42,15 +41,15 @@ end
   end
 
   def destroy
-      @tasks = Task.find(params[:id])
-    @tasks.destroy
+      @task = Task.find(params[:id])
+    @task.destroy
 
     flash[:success] = 'task は正常に削除されました'
     redirect_to tasks_url
       
   end
   private
-  def tasks_params
+  def task_params
     params.require(:task).permit(:content)
   end
 end
